78dc93bdc5c231e0bb9bc27f8a7a6ba72809208b
Lira Guedes <liracrg@gmail.com>
2023-03-23 19:37:10 -0300

    ultimos requisitos

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 const email = document.querySelector('#email');
 const pass = document.querySelector('#pass');
 const btnEntrar = document.querySelector('#Entrar');
+const btnSubmit = document.querySelector('#submit-btn');
+const agreement = document.querySelector('#agreement');
 
-    btnEntrar.addEventListener('click', (event) => {
-    event.preventDefault();
-    if (email.value === 'tryber@teste.com' && pass.value === '123456') {
-        alert('Olá, Tryber!');
+btnEntrar.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (email.value === 'tryber@teste.com' && pass.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+
+/*
+const activeSubmit = () => {
+    if (agreement.checked === true) {
+     btnSubmit.disabled = false;
     } else {
-        alert('Email ou senha inválidos.');
+    btnSubmit.disabled = true;
     }
-    });
+};
 
+agreement.addEventListener('click', activeSubmit);
+*/
 
+agreement.addEventListener('click', () => {
+    btnSubmit.disabled = !agreement.checked;
+})
+
+function textCount () {
+  const text = document.getElementById('text').value.length;
+  const counter = document.getElementById('counter');
+  counter.innerText = 500 - text;
+}
